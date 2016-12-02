01a9dfaadbf26f03babdca659212f4d9c87025db
Hernan Ruiz <hernan.ruiz@softtek.com>
2016-12-02 17:32:23 -0300

    Pestaña Mis Reservas(front): formato y popup de edición

--- src/pages/mis-reservas/mis-reservas.ts
+++ src/pages/mis-reservas/mis-reservas.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /*
   Generated class for the MisReservas page.
@@ -13,10 +14,42 @@ import { NavController } from 'ionic-angular';
 })
 export class MisReservasPage {
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {}
+
+  
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Día:',
+      inputs: [
+        {
+          name: 'Desde',
+		  placeholder: 'Desde'
+        },
+		{
+		  name: 'Hasta',
+		  placeholder: 'Hasta'
+		},
+      ],
+	  message: "*Esta cochera tiene una reserva a partir de las 14:00 por otro usuario",
+      buttons: [
+        {
+          text: 'Guardar',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Ocupar',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 
   ionViewDidLoad() {
     console.log('Hello MisReservasPage Page');
   }
-
 }
