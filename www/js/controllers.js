c5df1bfe23e7e1f1e7a2cc795b6df24fc594157a
Dario Cohen <dario.cohen@softtek.com>
2017-08-08 14:53:31 -0300

    removing www

--- www/js/controllers.js
+++
@@ -1,8 +0,0 @@
-angular.module('starter.controllers', [])
-
-.controller('InicioCtrl', function($scope) {})
-
-.controller('MisReservasCtrl', function($scope) {})
-
-.controller('CocherasCtrl', function($scope) {})
-
